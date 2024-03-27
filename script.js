6b35990e9c5cef963deb207ac93b417aba3192da
komal27kalsoom <94070291+komal27kalsoom@users.noreply.github.com>
2024-03-27 06:40:01 +0500

    new commit

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 
-
+// css class add on scroll and then
+// onscroll nav bar color and css change 
 const nav = document.querySelector("nav");
 
 window.addEventListener("scroll",function(){
     nav.classList.toggle("sticky",this.window.scrollY>0);
 })
+
+// ================
+let menu = document.querySelector('#menu-icon i');
+let navmenu = document.querySelector('.nav-menu ul');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-xmark')
+    navmenu.classList.toggle('open')
+}
+
+// <i class="fa-solid fa-xmark"></i>
